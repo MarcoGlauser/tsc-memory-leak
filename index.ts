61a04d45b0a3f7e3952b4f7602283cbb67ce40f3
MarcoGlauser <github@mglauser.ch>
2022-07-21 13:09:01 +0200

    further remove complexity from example

--- index.ts
+++ index.ts
@@ -1,43 +1,17 @@
-import {
-    Thing,
-    WithContext,
-    Product
-} from "schema-dts";
+import { Thing, WithContext, Product} from "schema-dts";
 
-export type Schema = Exclude<Thing, string>;
+export type ThingWithoutString = Exclude<Thing, string>;
 
-export const addContextToSchema = <T extends Schema>(schema: T): WithContext<T> => {
+export const addContextToSchema = <T extends ThingWithoutString>(schema: T): WithContext<T> => {
     return {
         '@context': 'https://schema.org',
         ...schema
     };
 };
 
-export const schema: Product = {
+export const product: Product = {
     "@type": "Product",
-    "name": "Lemosho Route",
-    "image": [],
-    "description": "The Lemosho is one of our favourite routes up Kilimanjaro as it has varied and beautiful scenery. It also has a high summit success rate!",
-    "mpn": "909833240",
-    "sku": "909833240",
-    "brand": {
-        "@type": "Organization",
-        "name": "Follow Alice",
-        "url": "https://followalice.com"
-    },
-    "offers": {
-        "@type": "Offer",
-        "priceCurrency": "USD",
-        "price": 2995,
-        "availability": "https://schema.org/InStock",
-        "priceValidUntil": "2023-07-19T11:55:05.803Z",
-        "url": "https://followalice.com/adventure-trips/climb-kilimanjaro/lemosho-route"
-    },
-    "aggregateRating": {
-        "@type": "AggregateRating",
-        "ratingValue": 5,
-        "reviewCount": 42
-    }
+    "name": "Some Product",
 }
 
-addContextToSchema(schema)
+addContextToSchema(product)
